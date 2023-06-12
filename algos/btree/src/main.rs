16b5df73ccda611d9c647907159082d29b51a04c
tsedlmeier <toni-sedlmeier@gmx.de>
2023-06-13 00:16:33 +0200

    fixed data structure

--- algos/btree/src/main.rs
+++ algos/btree/src/main.rs
@@ -1,60 +1,142 @@
-use std::collections::hash_map::Keys;
-use std::fs;
-use std::env;
+// use std::fs;
+// use std::env;
+use std::mem;
 
-fn read_seq(v: &mut Vec<i8>, path: &String, cnt: &mut i32)
-{
-    let data = fs::read_to_string(path).expect("Unable to read file");
-    for line in data.lines() {
-        (*cnt)+=1;
-        v.push( line.parse::<i8>().unwrap() );
-    }
-}
-
+// #[derive(Clone)]
 pub struct BTreeNode {
     m_: usize,
+    n_: usize,
     keys_: Vec<i32>,
-    refs_: Vec<Option<Box<BTreeNode>>>, // Option = None abbildung
+    refs_: Vec<BTreeNode>, // Option = None abbildung
     is_leaf_: bool
 }
+
+// Copyconstructor
+impl Clone for BTreeNode {
+    fn clone(&self) -> Self {
+        Self {
+            m_: self.m_,
+            n_: self.n_,
+            keys_: self.keys_.clone(),
+            refs_: self.refs_.clone(),
+            is_leaf_: self.is_leaf_,
+        }
+    }
+}
+
 impl BTreeNode {
-    fn new(m :usize) -> BTreeNode {
-        Self { m_: m, keys_: Vec::with_capacity(2*m-1), refs_: Vec::with_capacity(2*m), is_leaf_: (true) }
+    fn new(m :usize) -> Self {
+        Self { m_: m, n_: 0, keys_: Vec::with_capacity(2*m-1), refs_: Vec::with_capacity(2*m), is_leaf_: (true) }
     }
-    fn insert(&mut self, key: i32) {
-        if self.keys_.len() == self.m_ {
-            return;
-        }
-        else {
-            self.keys_.push(key);
-        }
-    }
-    fn walk(self) {
+    // For testing purpose
+    // fn insert(&mut self, key: i32) {
+    //     if self.keys_.len() == self.m_ {
+    //         println!("Keys storage full");
+    //         return;
+    //     }
+    //     else {
+    //         self.keys_.push(key);
+    //         self.n_ += 1;
+    //     }
+    // }
+
+    fn print_keys(&self) {
         print!("( ");
         for k in self.keys_.iter() {
             print!("{0} ", k);
         }
         print!(" )");
     }
+
+    fn walk(&self) {
+        if self.is_leaf_ {
+            self.print_keys();
+            return ;
+        }
+        for i in 0..self.n_ {
+            self.refs_[i].walk();
+        }
+    }
+
+    fn b_tree_split_child(&mut self, i: usize) {
+        let child = &mut self.refs_[i];
+        let mut h = BTreeNode::new(self.m_);
+        h.is_leaf_ = child.is_leaf_;
+        h.n_ = self.m_-1;
+
+        h.keys_.append(&mut child.keys_.split_off(self.m_));
+        // for j in 0..2*m-1 {
+        //     h.keys_[j] = child.keys_[j+m];
+        // }
+        if !child.is_leaf_ {
+            h.refs_.append(&mut child.refs_.split_off(self.m_));
+        }
+        let key_that_moves_to_parent = child.keys_.remove(self.m_ - 1);
+        self.keys_.insert(i, key_that_moves_to_parent);
+        self.refs_.insert(i + 1, h);
+    }
+
+    fn b_tree_insert_in_node (&mut self, key: i32) {
+        let mut i = self.n_;
+        if self.is_leaf_ {
+            while i >= 1 && key < self.keys_[i-1] {
+                i -= 1;
+            }
+            self.keys_.insert(i, key);
+            self.n_ += 1;
+        }
+        else {
+            let mut j = 0;
+            while j < self.n_ && key > self.keys_[j] {
+                j += 1;
+            }
+            if self.refs_[j].n_ == 2*self.m_-1 {
+                self.b_tree_split_child(j);
+                if key > self.keys_[j] {
+                    j += 1;
+                }
+            }
+            self.refs_[j].b_tree_insert_in_node(key);
+        }
+    }
 }
 
 struct BTree {
-    root_: Option<Box<BTreeNode>>
+    root_: BTreeNode
 }
+
 impl BTree {
-    fn new(root: Option<Box<BTreeNode>>) -> BTree {
-        BTree{ root_: root}
+    pub fn new(m: usize) -> Self {
+        Self { root_: BTreeNode::new(m) }
     }
+
     fn insert (&mut self, key: i32) {
-        // let r : Option<Box<BTreeNode>> = self.root_;
-        
+        let m = self.root_.m_;
+        if self.root_.keys_.len() == 2 * self.root_.m_ - 1 {
+            let h = mem::replace(&mut self.root_, BTreeNode::new(m));
+            self.root_.refs_.push(h);
+            self.root_.b_tree_split_child(0); // Call to split child node
+        }
+        self.root_.b_tree_insert_in_node(key);
+    }
+
+    fn walk(&self) {
+        self.root_.walk();
     }
 }
 
 fn main() {
-    let mut node : BTreeNode = BTreeNode::new(5);
-    node.insert(1);
-    node.insert(23);
-    node.insert(1231);
-    node.walk();
+    let mut b_tree : BTree = BTree::new(5);
+    b_tree.insert(1);
+    b_tree.insert(2);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.insert(3);
+    b_tree.walk();
 }
